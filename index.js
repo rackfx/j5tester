247ba482335d44f983193773c0038c86a7348c24
David Jones <david@drj.io>
2016-01-12 16:15:58 -0700

    f

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ board.on("ready", function() {
   var pin = new five.Pin(11);
   var photoSensor = new five.Pin('A0');
   var toggleSwitch = new five.Switch(11);
+  var ac = new five.Pin(5);
+  var acPolarity = new five.Pin(6);
   //  toggleSwitch.on("close", function() {
   //    console.log("closed");
   //  });
@@ -33,6 +35,36 @@ board.on("ready", function() {
 
   var i =0;
 
+  var accDir = 0;
+  var accOn = 0;
+
+  var iCount = 0
+
+  function acc(){
+
+    if (iCount == 0){
+      acPolarity.low();
+      ac.high();
+    }
+    else if (iCount == 10){
+      acPolarity.high();
+
+    }
+    else if (iCount == 20){
+
+      ac.low();
+    }
+    else if (iCount == 3){
+
+    }
+    else if (iCount == 100){
+      iCount =-1;
+    }
+    iCount ++
+  }
+
+  setInterval(acc, 50);
+
   setInterval(function(){ led.toggle(); }, 1000)
 
   function go(){
@@ -43,7 +75,7 @@ board.on("ready", function() {
 
       photoSensor.query(function(state2){
 
-        process.stdout.write("light state: " + state2.value + " switch state: " + state.value + "       \r");
+        process.stdout.write("light state: " + state2.value + " switch state: " + state.value +" icount: "+iCount+"       \r" );
         if((state.value==0) && (state2.value > 900)){
           stepper.ccw().step(128, function() {
             setTimeout(go,100);
